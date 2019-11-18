5304d2f8da845430bebe45410686901390e175ff
arun <mechanarun@gmail.com>
2019-11-18 14:17:48 +0530

    branch creation and pushing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { UserComponent } from './user/user.component';
 import { UserService } from './user/user.service';
 import { UserAsyncComponent } from './user-async/user-async.component';
 import { GitTestComponentComponent } from './git-test-component/git-test-component.component';
+import { Git2Component } from './git2/git2.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UserAsyncComponent, GitTestComponentComponent],
+  declarations: [AppComponent, UserComponent, UserAsyncComponent, GitTestComponentComponent, Git2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/git2/git2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git2',
+  templateUrl: './git2.component.html',
+  styleUrls: ['./git2.component.css']
+})
+export class Git2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
